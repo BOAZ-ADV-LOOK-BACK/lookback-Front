f6ce0d03afeaf6dcdd53ea77e369c46d1718e30a
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-12-18 01:42:00 +0900

    feat: 캘린더 동기화 UI 기능 추가
    - 네비게이션 메뉴에 캘린더 동기화 버튼 추가
    - 동기화 상태 표시를 위한 로딩 애니메이션 구현
    - 동기화 성공/실패 알림 구현
    - 기존 네비게이션 스타일과 일관성 유지

---
+++ components/component/icon/SyncIcon.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+
+export function SyncIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21.5 2v6h-6" />
+      <path d="M21.34 15.57a10 10 0 1 1-.57-8.38" />
+    </svg>
+  )
+}
